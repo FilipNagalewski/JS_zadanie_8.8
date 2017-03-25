be4380f9fc196d87247a9741c41eb772c6d68278
Filip Nagalewski <nagalewski@gmail.com>
2017-03-25 22:41:08 +0100

    minor change

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-  animal ='Zielone słonie';
+  animal = 'Zielone słonie';
   animalUpperCase = animal.toUpperCase();
   textReplace = text.replace('Papugi', animalUpperCase);
   textReplaceHalf = textReplace.substr(0, textReplace.length / 2);
